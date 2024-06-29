f2d37fbcb28ce3ea0a51f27f110f5f14c79d7606
sheedpro <san12williams@gmail.com>
2024-06-29 13:14:55 +0300

    created new pages

--- script.js
+++ script.js
@@ -60,3 +60,15 @@ drop4.addEventListener('click', function(){
         img4.src="assets/images/icon-minus.svg"
     }
 })
+
+let animation = anime({
+    targets: 'div',
+    // Properties 
+    translateX: 100,
+    borderRadius: 50,
+    // Property Parameters
+    duration: 2000,
+    easing: 'linear',
+    // Animation Parameters
+    direction: 'alternate'
+  }); 
